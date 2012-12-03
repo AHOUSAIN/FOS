2aac54794f6d1f8fe796bffeca88a57a118d0022
AHOUSAIN <afrahhassancmu@gmail.com>
2012-12-03 14:58:06 +0300

    Gemfile install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FOS::Application.config.secret_token = '33b6faf6460fb61c510d2ba292003e56d01931a56f9f2b24267ae4b6227569d953478275289179d75b966a238c41035cd53d4129bf90ecdc237b5d9fe800176a'
